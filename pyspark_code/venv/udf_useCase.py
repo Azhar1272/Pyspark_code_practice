90b170a6fcad958edf1313dbdcf8c01f1b4d6513
Azhar1272 <68108562+Azhar1272@users.noreply.github.com>
2024-12-03 12:29:41 +0530

    Update udf_useCase.py

--- pyspark_code/venv/udf_useCase.py
+++ pyspark_code/venv/udf_useCase.py
@@ -7,9 +7,11 @@ path_csv = "data/england_councils/district_councils.csv"
 def upperCase(col):
     return col.upper()
 
-upperCaseUDF = udf(upperCase, StringType())
+upperCaseUDF = udf(upperCase, StringType()) #spark.udf.register("upperCaseUDF", upperCase, StringType())
+
+
 
 df = spark_utils.read_csv(path_csv)
 #df.show()
-df2 = df.withColumn("country", upperCaseUDF(lit("abc")))
+df2 = df.withColumn("country", upperCaseUDF(lit("abc"))) # result = spark.sql("SELECT name, upperCaseUDF(name) AS name_upper FROM people")
 df2.show()
