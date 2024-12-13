b844d95d403cc696faa27378abd2381df0f762fb
Azhar1272 <68108562+Azhar1272@users.noreply.github.com>
2024-12-13 22:54:29 +0530

    Update test.py

--- pyspark_code/venv/Python_Code/test/unit/test.py
+++ pyspark_code/venv/Python_Code/test/unit/test.py
@@ -6,14 +6,3 @@ def max_val(input):
             max_val = input[key]
             max_key = key
     return max_key
-
-# input = {'x': -10, 'y': -5, 'z': -20}
-# output = max_val(input)
-# print(output)
-
-# math_operations.py
-# def add(a, b):
-#     return a + b
-
-# def subtract(a, b):
-#     return a - b
