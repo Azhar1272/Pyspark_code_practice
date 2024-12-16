d38dd11d166cdcb716f4fc8f8911fe516bb186ea
Azhar1272 <68108562+Azhar1272@users.noreply.github.com>
2024-12-16 14:25:04 +0530

    Update test_transform_data.py

--- pyspark_code/venv/Pyspark/integration_test/test_transform_data.py
+++ pyspark_code/venv/Pyspark/integration_test/test_transform_data.py
@@ -2,7 +2,11 @@ import pytest
 from pyspark.sql import SparkSession
 from glue_script import transform_data  # Replace with your actual module name
 
-@pytest.fixture(scope="module")
+# @pytest.fixture(scope="module") -- This means the fixture will run once for the entire module (the file containing the test functions), 
+# not per test. It's useful when you want to perform some expensive setup 
+# (like initializing a database connection) that doesn’t need to be done for every individual test, 
+# but only once for all the tests in the module.
+
 def spark():
     """Fixture to provide a SparkSession."""
     return SparkSession.builder.master("local").appName("pytest").getOrCreate()
@@ -16,8 +20,5 @@ def test_transform_data(spark):
     expected_data = [(2, "Alice"), (4, "Bob")]
     expected_df = spark.createDataFrame(expected_data, ["new_column", "name"])
 
-    # Apply transformation
-    result_df = transform_data(input_df)
-
     # Assert DataFrame equality
-    assert result_df.collect() == expected_df.collect()
+    assert transform_data(input_df).collect() == expected_df.collect()
