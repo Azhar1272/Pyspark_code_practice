957c823dc2f5fce4c8dc29cc56c74e46f8fa3fee
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-04-22 16:35:51 +0530

    Create add_two_user_provied_num.py

---
+++ pyspark_code/venv/Python_Code/add_two_user_provied_num.py
@@ -0,0 +1,12 @@
+# Problem description:
+# Given two numbers, add them and return the result.
+
+# Function code:
+def addNumbers(a, b):
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+
+# Function call and print output:
+print("The sum is", addNumbers(num1, num2))
