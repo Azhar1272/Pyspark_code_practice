c62755998f5ca913771a837e7d884600236f1918
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-04-22 16:31:53 +0530

    Update TwoSumProblem.py

--- pyspark_code/venv/Python_Code/TwoSumProblem.py
+++ pyspark_code/venv/Python_Code/TwoSumProblem.py
@@ -12,3 +12,22 @@ source = [2,3,4,9,8,6,1]
 target = 5
 out = find_number(source, target)
 print(out)
+
+
+
+'' Another way ''
+# Problem description:
+# Given a list and a target sum, return a list of indices where the sum of the elements at those indices is equal to the target.
+
+# Function code:
+def find_indices(input_list, target):
+    for i in range(len(input_list)):
+        for j in range(i + 1, len(input_list)):
+            if input_list[i] + input_list[j] == target:
+                return [i, j]
+
+input_list = [1, 5, 3, 6]
+target = 8
+# Function call and print output:
+print(find_indices(input_list, target))  # Expected output: [1, 2]
+
