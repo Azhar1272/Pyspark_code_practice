a75075baa5e1eaeec5fa7d89b6d257fce4c15ea7
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-04-22 16:32:59 +0530

    Update map_string_chars.py

--- pyspark_code/venv/Python_Code/map_string_chars.py
+++ pyspark_code/venv/Python_Code/map_string_chars.py
@@ -1,17 +1,17 @@
-"""
-Problem: Map characters from one string to another by index, avoiding remapping already mapped characters.
-"""
+# Problem description:
+# Given two strings, create a new string where each character from str1 is replaced by a corresponding character from str2. 
+# If a character from str1 is already mapped, do not change it.
 
+# Function code:
 str1 = 'hljljq'
 str2 = 'abcdef'
-
-char_map = {}
+map = {}
 final = ''
 
 for k, v in zip(str1, str2):
-    if k not in char_map:
-        char_map[k] = v
-    final += char_map[k]
+    if k not in map.keys():
+        map.update({k: v})
+    final += map[k]
 
-print("Character Map:", char_map)
-print("Mapped Output:", final)
+# Function call and print output:
+print(map, final)  # {'h': 'a', 'l': 'b', 'j': 'c', 'q': 'f'} abcbcf
