9cfb938329990e87b8615016c9b62ea99229d62d
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-04-30 18:40:04 +0530

    Update anagram.py

--- pyspark_code/venv/Python_Code/anagram.py
+++ pyspark_code/venv/Python_Code/anagram.py
@@ -1,18 +1,28 @@
-# check anagram in a given list
-
 def anagram(input):
-    sorted_word = ''
     dic = {}
-    for word in  input:
+    for word in input:
+        # Sort the word to get the key
         sorted_word = ''.join(sorted(word))
+        
+        # If the sorted word is already in the dictionary, append the word to the list
         if sorted_word in dic:
             dic[sorted_word].append(word)
         else:
-            dic[sorted_word] = [word] # both are same word needs to pass in list coz it is a list
-            dic.update({sorted_word:[word]}) # both are same word needs to pass in list coz it is a list
+            # If not, create a new list with the current word
+            dic[sorted_word] = [word]
     
-    return dic
+    # Convert the dictionary values (which are lists of anagrams) into a list of lists
+    result = list(dic.values())
+    
+    # Sort each group and return the sorted anagram groups
+    for group in result:
+        group.sort()
+    
+    return result
 
-input = ["eat", "tea", "dot", "car", "ate", "arc"]
-output = anagram(input)
-print(output) # {'aet': ['eat', 'tea', 'ate'], 'dot': ['dot'], 'acr': ['car', 'arc']}
+# Example usage
+input_words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+output = anagram(input_words)
+print(output)  #[['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat']]
+
+
