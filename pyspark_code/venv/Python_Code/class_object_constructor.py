b674c0131b3865344557b268f4f1105dac8c67d0
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-09 01:00:56 +0530

    Update class_object_constructor.py

--- pyspark_code/venv/Python_Code/class_object_constructor.py
+++ pyspark_code/venv/Python_Code/class_object_constructor.py
@@ -44,3 +44,41 @@ f = Field()
 f.set_value(42)
 print(f.get_value())  # Output: 42
 
+'''Example 3'''
+
+class SchoolMember:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class Teacher(SchoolMember):
+    def __init__(self, name: str, age: int, salary: int):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def show(self):
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}"
+
+
+class Student(SchoolMember):
+    def __init__(self, name: str, age: int, grades: int):
+        super().__init__(name, age)
+        self.grades = grades
+
+    def show(self):
+        return f"Name: {self.name}, Age: {self.age}, Grades: {self.grades}"
+
+
+teacher = Teacher("Mr.Snape", 40, 3000)
+print(teacher.name)  # "Mr.Snape"
+
+persons = [teacher, Student("Harry", 16, 75)]
+
+for person in persons:
+    print(person.show())
+
+
