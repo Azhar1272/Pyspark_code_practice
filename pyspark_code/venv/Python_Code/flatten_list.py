6a01f2b85f69c37557a615c5b52222d224ee3985
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-08 23:40:28 +0530

    Update flatten_list.py

--- pyspark_code/venv/Python_Code/flatten_list.py
+++ pyspark_code/venv/Python_Code/flatten_list.py
@@ -1,19 +1,12 @@
-"""
-Problem: Flatten a nested list.
-"""
-
-a = [1, [2, 4], [2, [4, 6]]]
+a = [1, [2, 4], [2, [4, 6]], {2,3}]
 final = []
 
 def flatlist(a):
     for x in a:
-        if not isinstance(x, list):
+        if not isinstance(x, (list, set)):
             final.append(x)
         else:
             flatlist(x)
     return final
 
 print("Flattened List:", flatlist(a))  
-
-#[1, 2, 4, 2, 4, 6]
-
