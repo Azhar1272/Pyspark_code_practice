2e2928fd77954bf36d76be6c33e9210fbe84b0a0
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-08 23:38:21 +0530

    Update find_substring_index.py

--- pyspark_code/venv/Python_Code/find_substring_index.py
+++ pyspark_code/venv/Python_Code/find_substring_index.py
@@ -1,21 +1,19 @@
-""Solution (without using built-in functions):""
+#""Solution (without using built-in functions):""
 
 def find_substring(s: str, sub: str) -> int:
-    # If both string and substring are empty, return 0
+  
     if s == "" and sub == "":
         return 0
     
     # If substring is empty, return 0 (empty substring is always found at index 0)
     if sub == "":
         return 0
-    
-    # Iterate through the string `s` and check for the substring `sub`
+  
     for i in range(len(s) - len(sub) + 1):
         # Check if the substring matches
         if s[i:i+len(sub)] == sub:
             return i  # Return the starting index
-    return -1  # Return -1 if substring is not found
-
+    return -1
 # Example usage
 s = "hello world"
 sub = "world"
