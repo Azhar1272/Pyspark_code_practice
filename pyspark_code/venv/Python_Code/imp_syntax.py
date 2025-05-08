5d98cbbd5a8bc8b8430e02f5af11fffa9ce2376c
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-09 00:26:18 +0530

    Create imp_syntax.py

---
+++ pyspark_code/venv/Python_Code/imp_syntax.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+	
+dic.update({item:len(lst)-1})
+dic[item] += len(lst)-1
+sorted_word = ''.join(sorted(word))  
+s.find(sub)	
+data['phone'].isdigit()
+re.match(email_pattern, data['email']):
+isinstance(x, (list,set))
+datetime.strptime("2024-02-01", "%d-%m-%Y") 
+random.randint(1,6)
+for k, v in zip(str1, str2)
+for i, x in enumerate(l):
+word = x.strip(",.!")
+for i, x in enumerate(l):
+    if x.startswith(sub):
+map(int, ['3', '4']) → Applies int() to each element:        
+clean =  ''.join(char for char in s if char.isalnum() or char == " ")
+------------------------------
+df = pd.DataFrame(data)
+final_result = df.groupby('product_id')['usd_price'].agg(['sum', 'mean']).reset_index().rename(columns={'sum': 'total_sales', 'mean': 'average_sales'})
+
