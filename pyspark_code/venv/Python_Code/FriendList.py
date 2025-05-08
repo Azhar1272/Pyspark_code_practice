149045e909d58e4b68e90a3fc1c09cfa556a7de2
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-08 22:05:56 +0530

    Update FriendList.py

--- pyspark_code/venv/Python_Code/FriendList.py
+++ pyspark_code/venv/Python_Code/FriendList.py
@@ -13,3 +13,20 @@ def output(fl):
 
 fl = [[1,2,3], [2,3,1,4], [4,3]]
 print(output(fl)) #{1: 5, 2: 5, 3: 6, 4: 4}
+
+
+'''
+#Another way to do this 
+
+def output(fl):
+    
+    final = {}
+    
+    for item in fl:
+        for x in item:
+            
+            final[x] = final.get(x,0)+len(item)-1
+            
+    return final 
+
+''
