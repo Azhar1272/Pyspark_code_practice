395a64bd42a4a1c81438cc67086cc7801001af3f
patelshweta020 <68934776+patelshweta020@users.noreply.github.com>
2025-05-21 21:32:27 +0530

    Create Mannual Sort.py

---
+++ pyspark_code/venv/Python_Code/Mannual Sort.py
@@ -0,0 +1,19 @@
+
+3. Input = [1,3,2,2,3,1] — Output = [1,1,2,2,3,3] (Without using sorting or bubble sort method)
+
+
+
+def manual_sort(nums):
+    count = {}
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+
+    result = []
+    for key in range(min(nums), max(nums)+1):
+        if key in count:
+            result.extend([key] * count[key])
+    return result
+
+# Example
+nums = [1, 3, 2, 2, 3, 1]
+print(manual_sort(nums))  # Output: [1, 1, 2, 2, 3, 3]
